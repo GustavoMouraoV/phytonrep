f3809c287530ef92bdde7152925cbccf5e78c729
GustavoMouraoV <gusvazmourao@gmail.com>
2025-02-26 00:13:20 +0000

    Add files via upload
    Rev of MyAvatar python script

--- MyAvatar.py
+++ MyAvatar.py
@@ -1,5 +1,6 @@
 #This script will create a character avatar based on user inputs
 
+#Print the Head of the Avatar
 def print_head(hair,eyes):
     print('')
     print(hair * 10)
@@ -10,17 +11,33 @@ def print_head(hair,eyes):
     print(' \ \'--\' /')
     print('  '+ ('-' * 6))
 
+#Print the torso of the avatar according to defined height
+def print_body(height,hand):
+    print((' ' * 4) + 'XX' + (' ' * 4))
+    print(hand + '|' + ('=' * 2) + '##' +  ('=' * 2) +  '|' + hand )
+    print((' ' * 4) + ('XX'))
+    i = 4
+    for index in range(1,height):
+        if (index <= i):
+            print((' ' * i) + ('XX' * index))
+            if (i > 0):
+                i = i - 1
+        else:
+            print('  '+ ('X' * 6))
+
 
 
 def main ():
     print("Welcome to MyAvatar")
-    #height = int(input("Please inform the height of your avatar: "))
+    height = int(input("Please inform the height of your avatar: "))
     hair = input("Please choose a character for the hair of your avatar: ")
     eyes = input("Please choose a character for the eyes of your avatar: ")
-    #hand = input("Please choose a character for the hand of your avatar: ")
+    hand = input("Please choose a character for the hand of your avatar: ")
     #shoe = input("Please choose four characters for the shoes of your avatar: ")
     #segment = (height - 11) // 2
     print_head(hair,eyes)
+    print_body(height,hand)
+
 
 
 main()
